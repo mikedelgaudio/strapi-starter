acc7c82b50dc4c2c2306461e1d3804741429ade0
Mike DelGaudio <delgaudiomike@gmail.com>
2023-05-18 21:30:10 -0700

    adding in changes for qs

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { getGlobalData } from "utils/api";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+App.getInitialProps = async (appContext: any): Promise<any> => {
+  // Calls page's `getInitialProps` and fills `appProps.pageProps`
+  const appProps = await App.getInitialProps(appContext);
+  const globalLocale = await getGlobalData(appContext.router.locale);
+
+  return {
+    ...appProps,
+    pageProps: {
+      global: globalLocale,
+    },
+  };
+};
+
+export default App;
